d87cdd501bf4c1080d248307fdbc8582e28742b4
Németh Balázs <nemeth.balazs.19f@szlgbp.com>
2022-01-31 11:03:54 +0100

    24-29 típusfeladat 1.0

---
+++ Balu_21-40.py
@@ -0,0 +1,31 @@
+from main import VBk
+
+with open("input.txt","r",encoding="utf8") as f:
+	for sor in f:
+		s = sor.strip().split("\t")
+		t = VBk(int(s[0]), s[1], int(s[2]), int(s[3]), s[4])
+
+def Döntőkikapásokszáma(ország):
+    hanyszorkapottki = 0
+    for vbk in VBk.lista:
+        if (vbk.helyezes == 2 and vbk.orszag == ország):
+            hanyszorkapottki+=1
+    print (f"{ország} {hanyszorkapottki}-szer/ször/szor kapott ki a döntőben")
+
+print("24) Hányszor kapott ki a döntőben Magyarország?")
+Döntőkikapásokszáma("Magyarország")
+
+print("25) Hányszor kapott ki a döntőben Mongólia?")
+Döntőkikapásokszáma("Mongólia")
+
+print("26) Hányszor kapott ki a döntőben Svájc?")
+Döntőkikapásokszáma("Svájc")
+
+print("27) Hányszor kapott ki a döntőben Brazília?")
+Döntőkikapásokszáma("Brazília")
+
+print("28) Hányszor kapott ki a döntőben Németország?")
+Döntőkikapásokszáma("Németország")
+
+print("29) Hányszor kapott ki a döntőben Argentína?")
+Döntőkikapásokszáma("Argentína")
--- main.py
+++ main.py
@@ -7,8 +7,3 @@ class VBk:
         self.ev = vbev
         self.hely = vbhely
         VBk.lista.append(self)
-
-with open("input.txt","r",encoding="utf8") as f:
-	for sor in f:
-		s = sor.strip().split("\t")
-		t = VBk(int(s[0]), s[1], int(s[2]), int(s[3]), s[4])
