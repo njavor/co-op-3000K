ed6dc3c35101fd1813e703616843759db5212fc7
Niki Jávor <nikolettjavor@gmail.com>
2022-02-01 10:39:18 +0100

    Donto [53-57]

--- niki.py
+++ niki.py
@@ -2,14 +2,17 @@ from main import VBk
 
 def KiNyert(lista, rendezo):
     szotar = {}
+    voltVB = False
     for elem in lista:
         if elem.hely == rendezo and elem.helyezes == 1:
             if elem.orszag in szotar.keys():
                 szotar[elem.orszag] +=1
             else:
                 szotar[elem.orszag] = 1
+                voltVB = True
     for orszag, amount in szotar.items():
         print("{}: {}".format(orszag, amount))
+    if not voltVB: print("Ebben az országban nem volt VB rendezve")
 
 def DobNyert(lista, dobogos):
     nyertesek = {}    
@@ -21,6 +24,20 @@ def DobNyert(lista, dobogos):
                 nyertesek[elem.ev] = elem.orszag
         if elem.orszag == dobogos and (elem.helyezes == 1 or elem.helyezes == 2 or elem.helyezes == 3):
             print(nyertesek[elem.ev])
+
+def Donto(lista, orszag):
+    evek = []
+    for elem in lista:
+        if elem.orszag == orszag and (elem.helyezes == 1 or elem.helyezes == 2):
+            evek.append(elem.ev)
+    i = 0
+    if not len(evek) == 0: 
+        while i < len(lista) and not len(evek) == 0:
+            if lista[i].ev == evek[0] and not lista[i].orszag == orszag:
+                print(f"{lista[i].orszag} - {evek[0]}")
+                evek.remove(evek[0])
+            i+=1
+    else: print("Ez az ország nem játszott döntőt.")
     
 
 print("42)	A program olvasson be egy csapat nevet és írja ki, a csapat vb-n elért legjobb helyezését!")
@@ -47,10 +64,17 @@ print("52)	Melyik csapat nyerte a vb-t, amikor Argentína dobogós helyzést ér
 DobNyert(VBk.lista, "Argentína")
 
 print("53)	Kikkel játszott döntőt Magyarország? Az ellenfél csapat neve mellett az évszámot is írja ki!")
+Donto(VBk.lista, "Magyarország")
 print("54)	Kikkel játszott döntőt Mongólia? Az ellenfél csapat neve mellett az évszámot is írja ki!")
+Donto(VBk.lista, "Mongólia")
 print("55)	Kikkel játszott döntőt Svájc? Az ellenfél csapat neve mellett az évszámot is írja ki!")
+Donto(VBk.lista, "Svájc")
 print("56)	Kikkel játszott döntőt Barzília? Az ellenfél csapat neve mellett az évszámot is írja ki!")
+Donto(VBk.lista, "Brazília")
 print("57)	A program olvasson be egy ország nevet és írja ki, kikkel játszott döntőt az illető csapat? Az ellenfél csapat neve mellett az évszámot is írja ki!")
+inp = input("Írd be egy ország nevét:  ")
+Donto(VBk.lista, inp)
+
 print("58)	Melyik csapat nyert többször is vb-t?")
 print("59)	Melyik országban rendeztek többször is vb-t?")
 print("60)	Melyik csapat(ok) nyert a legtöbbször vb-t? A csapat neve mellett a vb gyözelmmek számát is írja ki! ")
